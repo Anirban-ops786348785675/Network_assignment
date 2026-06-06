09a1c03521303f0227c13cd286b9c9a1723b1282
Anirban-ops786348785675 <sethanirban69677575@gmail.com>
2026-06-06 12:04:34 +0530

    Update bitStuffingPython.py
    By Anirban003

--- bitStuffingPython.py
+++ bitStuffingPython.py
@@ -1,10 +1,58 @@
-signal='101011111111'
-l=len(signal)   #counting the total bits in signal
-i=0
-while i<(l-5):
-    if signal[i:i+5]=='11111':                  #if we get continuous five 1's 
-        signal=signal[:i+5]+'0'+signal[i+5:]    # then we append a '0' after that
-        i+=5                                    #and increase the i pointer to 5
-    else: i+=1      #else increase the i pointer to 1
-print(signal)
-#output 1010111110111
+def bitStuffing(N, arr):
+
+   
+    brr = [0 for _ in range(30)]
+
+    
+    k = 0
+    i = 0
+    j = 0
+
+   
+    while (i < N):
+
+        
+        if (arr[i] == 1):
+
+            count = 1
+
+          
+            brr[j] = arr[i]
+
+            k = i + 1
+            while True:
+                if not (k < N and arr[k] == 1 and
+                        count < 5):
+                    break
+
+                j += 1
+                brr[j] = arr[k]
+                count += 1
+
+                
+                if (count == 5):
+                    j += 1
+                    brr[j] = 0
+
+                i = k
+                k += 1
+
+      
+        else:
+            brr[j] = arr[i]
+
+        i += 1
+        j += 1
+
+
+    for i in range(0, j):
+        print(brr[i], end="")
+
+
+
+if __name__ == "__main__":
+
+    N = 6
+    arr = [1, 1, 1, 1, 1, 1]
+
+    bitStuffing(N, arr)
